84d95e2ee481af324c8b4a668f9b5b496b386d5c
Daniel Seif <daniel.seif@contorion.de>
2017-09-21 14:18:02 +0200

    changed salt version to 2017.7

--- scripts/salt.sh
+++ scripts/salt.sh
@@ -1,4 +1,4 @@
 apt-get -yq install libzmq3 libzmq3-dev python-zmq
 
 curl https://bootstrap.saltstack.com/stable/bootstrap-salt.sh -o /tmp/install_salt.sh
-sh /tmp/install_salt.sh -P git v2015.8.1
+sh /tmp/install_salt.sh -P git v2017.7.0
